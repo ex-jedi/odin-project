daf935289268ecc73775ee0d585c8b782b587a70
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-19 16:50:26 +0100

    Add and remove with animations

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -8,13 +8,33 @@
 
 const addElementButton = document.querySelector('.add-element-button');
 let count = 6;
+const wrapper = document.querySelector('.wrapper');
 
 function addItem() {
-  const wrapper = document.querySelector('.wrapper');
-  const newElement = `<div class='added-div'>${count}</div>`;
-  console.log(count);
-  wrapper.insertAdjacentHTML('beforeend', newElement);
+  const newElement = document.createElement('div');
+  newElement.classList.add('items', 'appear');
+  newElement.innerText = count;
+  wrapper.insertAdjacentElement('beforeend', newElement);
   count += 1;
 }
 
 addElementButton.addEventListener('click', addItem);
+
+function removeItem(e) {
+  // Get element which was clicked on
+  const targetElement = e.target;
+  // If it's an item, delete it
+  if (targetElement.className === 'items') {
+    targetElement.remove();
+  }
+  // Get remaining Elements
+  const itemsList = document.querySelectorAll('.items');
+  itemsList.forEach((item, index) => {
+    item.classList.remove('appear');
+    item.addEventListener('animationend', () => item.classList.remove('animate'), { once: true });
+    item.innerText = index + 1;
+    item.classList.add('animate');
+  });
+}
+
+wrapper.addEventListener('click', removeItem);
