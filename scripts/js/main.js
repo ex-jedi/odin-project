dfad710320c91bc8c250b0e8309ab71089f62e10
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-19 15:46:06 +0100

    Unknown number of items with CSS Grid

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -6,3 +6,15 @@
 // ** Imported  **
 // *==============================================================================
 
+const addElementButton = document.querySelector('.add-element-button');
+let count = 6;
+
+function addItem() {
+  const wrapper = document.querySelector('.wrapper');
+  const newElement = `<div class='added-div'>${count}</div>`;
+  count++;
+  console.log(count);
+  wrapper.insertAdjacentHTML('beforeend', newElement);
+}
+
+addElementButton.addEventListener('click', addItem);
