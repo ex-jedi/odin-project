74036dbc6f4b0a61e30d8802280ae3555ec5d665
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-08-01 14:58:16 +0100

    Changing js file path

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -19,3 +19,19 @@ console.log(myObject.anotherProperty);
 // Dot notation
 // eslint-disable-next-line dot-notation
 console.log(myObject['anotherProperty']);
+
+// Object constructor
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function () {
+    console.log(this.name);
+  };
+}
+
+const playerOne = new Player('Dug', 'X');
+console.log(playerOne.name);
+
+const heading = document.querySelector('H1');
+console.log(heading);
--- scripts/minified/main-min.js
+++ scripts/minified/main-min.js
@@ -1,2 +1,44 @@
-!function(){const o={property:"Value",anotherProperty:88,method(){}};console.log(o.anotherProperty),console.log(o.anotherProperty)}();
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+// *==============================================================================
+// ** Imports  **
+// *==============================================================================
+
+// *==============================================================================
+// ** Imported  **
+// *==============================================================================
+
+const myObject = {
+  property: 'Value',
+  anotherProperty: 88,
+  method() {
+    // Stuff
+  },
+};
+// Dot notation
+console.log(myObject.anotherProperty);
+
+// Dot notation
+// eslint-disable-next-line dot-notation
+console.log(myObject['anotherProperty']);
+
+// Object constructor
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function () {
+    console.log(this.name);
+  };
+}
+
+const playerOne = new Player('Dug', 'X');
+console.log(playerOne.name);
+
+const heading = document.querySelector('H1');
+console.log(heading);
+
+
+/******/ })()
+;
 //# sourceMappingURL=main-min.js.map
