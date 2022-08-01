7c123effe29ffe461f6f52351aa00759e660e38c
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-08-01 12:20:25 +0100

    Clamp with readout

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -5,4 +5,14 @@
 // *==============================================================================
 // ** Imported  **
 // *==============================================================================
-
+const widthDiv = document.querySelector('.clamp-div');
+widthDiv.getBoundingClientRect();
+console.log('🚀 ~ file: main.js ~ line 10 ~ widthDiv.getBoundingClientRect()', widthDiv.getBoundingClientRect());
+widthDiv.innerHTML = `
+<p>Width = ${widthDiv.getBoundingClientRect().width.toFixed()}px.</p>
+`;
+window.addEventListener('resize', () => {
+  widthDiv.innerHTML = `
+<p>Width = ${widthDiv.getBoundingClientRect().width.toFixed()}px.</p>
+`;
+});
